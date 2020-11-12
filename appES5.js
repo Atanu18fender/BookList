2e1d046a492c2a403ba486cfe1defaabfa5e09b6
Atanu Raul <atanuraul2016@gmail.com>
2020-11-12 19:47:44 +0530

    ES5.1

---
+++ appES5.js
@@ -0,0 +1,54 @@
+// Book constructor 
+function Book(title,author,isbn){
+    this.title=title;
+    this.author=author;
+    this.isbn=isbn;
+}
+
+// UI constructor
+function UI(){}
+
+// Add Book To List
+UI.prototype.addBookToList = function(book){
+    const list = document.getElementById('book-list');
+    // Create tr element
+    const row = document.createElement('tr');
+    
+    // Insert cols
+    row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href='#' class='delete'>X</a></td>
+    `;
+
+    list.appendChild(row); 
+
+}
+
+// Clear field method
+UI.prototype.clearFields=function(){
+    document.getElementById('title').value='';
+    document.getElementById('author').value='';
+    document.getElementById('isbn').value='';
+}
+
+// Event Listeners
+document.getElementById('book-form').addEventListener('submit',function(e){
+    // get form values
+    const title=document.getElementById('title').value,
+    author=document.getElementById('author').value,
+    isbn=document.getElementById('isbn').value;
+ 
+// Instatiate Book    
+const book=new Book(title,author,isbn);
+
+// Instatiate UI
+const ui=new UI();
+ui.addBookToList(book);
+
+// Clear Fields
+ui.clearFields();
+
+e.preventDefault();
+})
